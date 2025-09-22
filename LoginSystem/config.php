c9f933e65c870c905ab03c66373eba14d4227014
Leebug4 <94907437+Leebug4@users.noreply.github.com>
2025-09-22 17:19:14 +0800

    update

--- LoginSystem/config.php
+++ LoginSystem/config.php
@@ -8,7 +8,7 @@ $password = "6bc9799e4d09a1b259c25cb58209afe5";
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "✅ Database connected successfully!";
+    // echo "Database connected successfully!";
 } catch (PDOException $e) {
     die("❌ Connection failed: " . $e->getMessage());
 }
